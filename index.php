2c3efb3b12a83f1d40e493e5ebfc092d6a0918df
norebese <ehddls1029@naver.com>
2025-04-30 00:19:52 +0900

    filter good

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <?php include 'mainmenu/common/header.php'; ?>
 
     <!-- DB 연결 -->
-    <?php require_once 'mainmenu/queries/get_new_products.php';?>
+    <?php require_once 'mainmenu/queries/get_index_products.php';?>
 
     <!-- Main Content -->
     <main class="main-content">
@@ -23,10 +23,6 @@
                 <div class="banner-content">
                     <h1>KISIA SHOP</h1>
                     <p>최고의 품질과 서비스를 제공합니다</p>
-                    <!-- <div class="banner-buttons">
-                        <a href="mainmenu/all.php" class="btn-shop">쇼핑하기</a>
-                        <a href="mainmenu/all.php" class="btn-event">이벤트 보기</a>
-                    </div> -->
                 </div>
             </div>
         </section>
@@ -43,8 +39,8 @@
                     </div>
                     <div class="product-grid">
                         <?php
-                        if (mysqli_num_rows($get_new_products) > 0) {
-                            while ($row_best = mysqli_fetch_assoc($get_new_products)) {
+                        if (mysqli_num_rows($get_keyboard_products) > 0) {
+                            while ($row_best = mysqli_fetch_assoc($get_keyboard_products)) {
                                 // 상품 하나당 하나의 카드 출력
                                 echo '<div class="product-card">';
                                 echo '<img src="' . htmlspecialchars($row_best['image_url']) . '" alt="' . htmlspecialchars($row_best['name']) . '">';
@@ -71,7 +67,22 @@
                             <a href="mainmenu/mouse.php" class="more">더 보기</a>
                         </div>
                     </div>
-                    <p class="section-description">최고의 마우스를 만나보세요</p>
+                    <div class="product-grid">
+                        <?php
+                        if (mysqli_num_rows($get_mouse_products) > 0) {
+                            while ($row_best = mysqli_fetch_assoc($get_mouse_products)) {
+                                // 상품 하나당 하나의 카드 출력
+                                echo '<div class="product-card">';
+                                echo '<img src="' . htmlspecialchars($row_best['image_url']) . '" alt="' . htmlspecialchars($row_best['name']) . '">';
+                                echo '<h3>' . htmlspecialchars($row_best['name']) . '</h3>';
+                                echo '<p class="price">' . number_format($row_best['price']) . '원</p>';
+                                echo '</div>';
+                            }
+                        } else {
+                            echo '<p>등록된 상품이 없습니다.</p>';
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
         </section>
@@ -86,7 +97,22 @@
                             <a href="mainmenu/mousepad.php" class="more">더 보기</a>
                         </div>
                     </div>
-                    <p class="section-description">최고의 마우스 패드를 만나보세요</p>
+                    <div class="product-grid">
+                        <?php
+                        if (mysqli_num_rows($get_mousepad_products) > 0) {
+                            while ($row_best = mysqli_fetch_assoc($get_mousepad_products)) {
+                                // 상품 하나당 하나의 카드 출력
+                                echo '<div class="product-card">';
+                                echo '<img src="' . htmlspecialchars($row_best['image_url']) . '" alt="' . htmlspecialchars($row_best['name']) . '">';
+                                echo '<h3>' . htmlspecialchars($row_best['name']) . '</h3>';
+                                echo '<p class="price">' . number_format($row_best['price']) . '원</p>';
+                                echo '</div>';
+                            }
+                        } else {
+                            echo '<p>등록된 상품이 없습니다.</p>';
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
         </section>
@@ -101,7 +127,22 @@
                             <a href="mainmenu/accessory.php" class="more">더 보기</a>
                         </div>
                     </div>
-                    <p class="section-description">최고의 악세서리를 만나보세요</p>
+                    <div class="product-grid">
+                        <?php
+                        if (mysqli_num_rows($get_accessory_products) > 0) {
+                            while ($row_best = mysqli_fetch_assoc($get_accessory_products)) {
+                                // 상품 하나당 하나의 카드 출력
+                                echo '<div class="product-card">';
+                                echo '<img src="' . htmlspecialchars($row_best['image_url']) . '" alt="' . htmlspecialchars($row_best['name']) . '">';
+                                echo '<h3>' . htmlspecialchars($row_best['name']) . '</h3>';
+                                echo '<p class="price">' . number_format($row_best['price']) . '원</p>';
+                                echo '</div>';
+                            }
+                        } else {
+                            echo '<p>등록된 상품이 없습니다.</p>';
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
         </section>
