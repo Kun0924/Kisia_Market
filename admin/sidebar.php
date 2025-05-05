71f209d15e98c99b45d77da4313e8190330b42a4
Kun0924 <42yunakim@gmail.com>
2025-05-06 04:43:37 +0900

    관리자페이지리뉴얼

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -5,43 +5,43 @@
     </div>
     <nav class="admin-menu">
         <ul>
-            <li <?php echo $_SERVER['PHP_SELF'] == 'dashboard_main.php' ? 'class="active"' : ''; ?>>
+            <li>
                 <a href="dashboard_main.php">
                     <i class="fas fa-home"></i>
                     <span>대시보드</span>
                 </a>
             </li>
-            <li <?php echo $_SERVER['PHP_SELF'] == 'members.php' ? 'class="active"' : ''; ?>>
+            <li>
                 <a href="members.php">
                     <i class="fas fa-users"></i>
                     <span>회원 관리</span>
                 </a>
             </li>
-            <li <?php echo $_SERVER['PHP_SELF'] == 'products.php' ? 'class="active"' : ''; ?>>
+            <li>
                 <a href="products.php">
                     <i class="fas fa-cube"></i>
                     <span>상품 관리</span>
                 </a>
             </li>
-            <li <?php echo basename($_SERVER['PHP_SELF']) == 'approve.php' ? 'class="active"' : ''; ?>>
+            <li>
                 <a href="approve.php">
                     <i class="fas fa-shopping-cart"></i>
                     <span>주문 관리</span>
                 </a>
             </li>
-            <li <?php echo $_SERVER['PHP_SELF'] == 'reviews.php' ? 'class="active"' : ''; ?>>
+            <li>
                 <a href="reviews.php">
                     <i class="fas fa-pencil"></i>
                     <span>리뷰 관리</span>
                 </a>
             </li>
-            <li <?php echo $_SERVER['PHP_SELF'] == 'notices.php' ? 'class="active"' : ''; ?>>
+            <li>
                 <a href="notices.php">
                     <i class="fas fa-bullhorn"></i>
                     <span>공지사항 관리</span>
                 </a>
             </li>
-            <li <?php echo $_SERVER['PHP_SELF'] == 'inquiries.php' ? 'class="active"' : ''; ?>>
+            <li>
                 <a href="inquiries.php">
                     <i class="fas fa-question-circle"></i>
                     <span>문의사항 관리</span>
