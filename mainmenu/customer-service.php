dcc7e9a5f945c2c1f7e9d3e7d4a3ca014ce474ac
norebese <ehddls1029@naver.com>
2025-05-16 17:51:55 +0900

    경로 수정정

--- mainmenu/customer-service.php
+++ mainmenu/customer-service.php
@@ -43,7 +43,7 @@
                         <?php while ($row = mysqli_fetch_assoc($get_notice)): ?>
                             <div class="qna-item notice-item">
                                 <div class="qna-info">
-                                    <a href="notice_detail.php?id=<?= $row['id'] ?>" class="qna-title"><?= htmlspecialchars($row['title']) ?></a>
+                                    <a href="notice_detail.php?id=<?= $row['id'] ?>" class="qna-title"><?= $row['title'] ?></a>
                                     <span class="qna-meta"><?= $row['id'] ?></span>
                                     <span class="qna-user">관리자</span>
                                     <span class="qna-date"><?= date('Y-m-d', strtotime($row['created_at'])) ?></span>
--- mainmenu/queries/get_user_id_pw.php
+++ mainmenu/queries/get_user_id_pw.php
@@ -38,7 +38,7 @@
 
                 $mail->isHTML(true);
                 $mail->Subject = '비밀번호 재설정 안내';
-                $mail->Body    = '비밀번호를 재설정하려면 <a href="/mainmenu/reset_password.php?email=' . $email . '&profile=' . $profile . '">여기</a>를 클릭하세요.';
+                $mail->Body    = '비밀번호를 재설정하려면 <a href="http://kisia-shop1.koreacentral.cloudapp.azure.com/mainmenu/reset_password.php?email=' . $email . '&profile=' . $profile . '">여기</a>를 클릭하세요.';
 
                 $mail->send();
             } catch (Exception $e) {
