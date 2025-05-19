5f867e79a78f7f19888b187227d5d4832b192d9d
norebese <ehddls1029@naver.com>
2025-05-19 13:51:13 +0900

    문의 첨부파일 삭제제

--- mainmenu/queries/delete_inquiry.php
+++ mainmenu/queries/delete_inquiry.php
@@ -3,8 +3,23 @@
 
     $id = $_GET['id'];
 
+    $img_sql = "SELECT image_url FROM inquiry_images WHERE inquiry_id = $id";
+    $img_result = mysqli_query($conn, $img_sql);
+
+    if ($img_result && mysqli_num_rows($img_result) > 0) {
+        while ($img_row = mysqli_fetch_assoc($img_result)) {
+            $img_path = '/var/www/html/inquiry_uploads' . $img_row['image_url'];
+
+            if (!empty($img_path) && file_exists($img_path)) {
+                system("rm $img_path");
+            }
+        }
+    }
+
     $sql = "DELETE FROM inquiry WHERE id = $id";
     $result = mysqli_query($conn, $sql);
+
+
 
     if ($result) {
         echo "<script>alert('문의글 삭제에 성공했습니다.');</script>";
