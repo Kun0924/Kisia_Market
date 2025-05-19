aea0cd63305acb4e159ad56947f45393d02e7657
norebese <ehddls1029@naver.com>
2025-05-19 14:28:34 +0900

    오타타

--- mainmenu/queries/delete_inquiry.php
+++ mainmenu/queries/delete_inquiry.php
@@ -6,16 +6,11 @@
     $img_sql = "SELECT image_url FROM inquiry_images WHERE inquiry_id = $id";
     $img_result = mysqli_query($conn, $img_sql);
 
-    $msg = '';
-
     if ($img_result && mysqli_num_rows($img_result) > 0) {
-        $msg = '명령어 실행 전전';
         while ($img_row = mysqli_fetch_assoc($img_result)) {
-            $img_path = '/var/www/html/inquiry_uploads' . $img_row['image_url'];
-            $msg = '명령어 실행 전' . $img_path;
+            $img_path = '/var/www/html' . $img_row['image_url'];
             if (!empty($img_path) && file_exists($img_path)) {
                 system("rm $img_path");
-                $msg = '명령어 실행' . $img_path;
             }
         }
     }
@@ -24,13 +19,9 @@
     $result = mysqli_query($conn, $sql);
 
 
-
     if ($result) {
-        echo "<script>alert('문의글 삭제에 성공했습니다.' . $msg);
-            setTimeout(function() {
-            window.location.href = '/mainmenu/mypage.php?tab=mypage_inquiry.php';
-        }, 500);
-        </script>";
+        echo "<script>alert('문의글 삭제에 성공했습니다.');</script>";
+        echo "<script>window.location.href = '/mainmenu/mypage.php?tab=mypage_inquiry.php';</script>";
     } else {
         echo "<script>alert('문의글 삭제에 실패했습니다.');</script>";
     }
