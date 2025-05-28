8881a33f8ae78967e311a7e171a31d056d94dd7a
Kun0924 <42yunakim@gmail.com>
2025-05-28 14:49:34 +0900

    errorpage

---
+++ errorpage/403.php
@@ -0,0 +1,20 @@
+<?php
+http_response_code(403);
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>접근 거부 - KISIA SHOP</title>
+    <link rel="stylesheet" href="css/errorpage.css">
+</head>
+<body>
+<main class="error-main-content">
+    <div class="error-container">
+        <h1>403</h1>
+        <p>이 페이지에 접근할 권한이 없습니다.</p>
+        <a href="/" class="btn-home">홈으로 돌아가기</a>
+    </div>
+</main>
+</body>
+</html>
