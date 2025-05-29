cacd2d2c97eb0026a685580033baa456453649eb
norebese <ehddls1029@naver.com>
2025-05-29 16:51:01 +0900

    f

--- mainmenu/queries/download.php
+++ mainmenu/queries/download.php
@@ -1,7 +1,7 @@
 <?php
 $file = $_GET['file'] ?? '';
 $file = basename($file); // 디렉터리 트래버설 방지
-$path = "/root/Kisia_Market/inquiry_uploads/" . $file;
+$path = "/var/www/html/inquiry_uploads/" . $file;
 
 if(file_exists($path)) {
     header('Content-Description: File Transfer');
